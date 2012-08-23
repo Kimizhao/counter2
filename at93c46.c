ebf50dc701a818365fac2b3f835f5083ad7065e2
Kimi <gifly.zhao@gmail.com>
2012-08-24 00:26:50 +0800

    at93c66

--- at93c46.c
+++ at93c46.c
@@ -1,9 +1,9 @@
 #include "common.h"
 
-sbit CS=P2^0;
-sbit SK=P2^1;
-sbit DI=P2^2;
-sbit DO=P2^3;
+sbit CS=P1^0;
+sbit SK=P1^1;
+sbit DI=P1^2;
+sbit DO=P1^3;
 
 void delay(void)
 {
@@ -22,6 +22,10 @@ void shift1(uchar num,uchar sdata)
 		sdata<<=1;
 		DI=CY;
 		SK=1;
+		// sdata<<=1;
+		// DI=CY;
+		// SK=1;
+		// SK=0;
 	}
 }
 
@@ -37,12 +41,17 @@ void shift2(uchar num,uchar sdata)
 		sdata<<=1;
 		DI=CY;
 		SK=1;
+		// sdata<<=1;
+		// DI=CY;
+		// SK=1;
+		// SK=0;
 	}
 }
 void write_en(void)
 {
     uchar sb_op=0x80,add=0xc0;
     CS=0;
+	SK=0;//
     CS=1;
     shift1(3,sb_op);
 	shift1(7,add);
@@ -134,48 +143,48 @@ void ew_disable(void)
 	delay();
 }
 
-void main(void)
-{	
-   	uchar address=0x00,i;
-	uchar data1[]={0xfe,0xfd,0xfb,0xf7};
-    write_en();
-	write(0x00,data1[0]);
-	P1=data1[0];
-	delay();
-	write(0x01,data1[1]);
-	P1=data1[1];
-	delay();
-	write(0x02,data1[2]);
-	P1=data1[2];
-	delay();
-	write(0x03,data1[3]);
-	P1=data1[3];
-	delay();
+// void main(void)
+// {	
+   	// uchar address=0x00,i;
+	// uchar data1[]={0xfe,0xfd,0xfb,0xf7};
+    // write_en();
+	// write(0x00,data1[0]);
+	// P1=data1[0];
+	// delay();
+	// write(0x01,data1[1]);
+	// P1=data1[1];
+	// delay();
+	// write(0x02,data1[2]);
+	// P1=data1[2];
+	// delay();
+	// write(0x03,data1[3]);
+	// P1=data1[3];
+	// delay();
 
-	/*
-	for(i=0;i<4;i++)
-	{  P1=data1[i];
-	   write(address,data1[i]);
-	   address++;
-	   delay();
-	} */
-   	P1=read(0x00);
-	delay();
-	P1=read(0x01);
-	delay();
-	P1=read(0x02);
-	delay();
-	P1=read(0x03);
-	delay();
+	// /*
+	// for(i=0;i<4;i++)
+	// {  P1=data1[i];
+	   // write(address,data1[i]);
+	   // address++;
+	   // delay();
+	// } */
+   	// P1=read(0x00);
+	// delay();
+	// P1=read(0x01);
+	// delay();
+	// P1=read(0x02);
+	// delay();
+	// P1=read(0x03);
+	// delay();
 
 
 
-/*	address=0x00;
-	for(i=0;i<4;i++)
-	{
-	   P1=read(address);
-	   delay(); 
-	   address++;
-	 } */
-	while(1);
-}
+// /*	address=0x00;
+	// for(i=0;i<4;i++)
+	// {
+	   // P1=read(address);
+	   // delay(); 
+	   // address++;
+	 // } */
+	// while(1);
+// }
--- main.c
+++ main.c
@@ -16,15 +16,23 @@ unsigned char DisSingle;
 unsigned int curnum;
 unsigned int num;
 
-sbit CNUP=P1^0;
-sbit CNDOWN=P1^1;
-sbit CNNUN=P1^3;
+//sbit CNUP=P1^0;
+// sbit CNDOWN=P1^1;
+// sbit CNNUN=P1^3;
 bit newa,newb;
 
 void CntScaner(void);
 void delay(unsigned int cnt);
 
 extern void SerialDataOutput(unsigned char Data);//data为C51关键字
+
+//extern void write(uchar wr_add,uchar wr_data);
+//extern uchar read(uchar rd_add);
+//extern void write_en(void);
+extern void EEPROM_Write_Enable(void);
+extern void EEPROM_Write(unsigned char Addr,unsigned char Data);
+extern unsigned char EEPROM_Read(unsigned char Addr);
+
 
 void delay(unsigned int cnt)
 {
@@ -40,7 +48,6 @@ void Init_Timer0(void)
 	ET0=1;                     /* enable timer0 interrupt */
 	TR0=1;  
 }
-
 
 void Init_counter2(void)
 {
@@ -66,7 +73,7 @@ void main(void)
 	//unsigned int num;
 	/* 定时/计数器2 作为外部脉冲计数初始化 */
 	//Init_counter2();
-	Init_Timer0();
+	//Init_Timer0();
 	
 	curnum=0;
 
@@ -77,16 +84,52 @@ void main(void)
 	DisHunder=table[(num/100)%10];
 	DisThous=table[(num/1000)%10];
 	
+	//EEPROM_Write_Enable();
+	//delay(2000);
+	//EEPROM_Write(0x00,0x3f);
+	
+	P0=0x3f;
+	delay(60000);
+	
+	P0=EEPROM_Read(0x00);
+	P2=0;
+	
+	// write_en();
+	// wr_all(0x66);
+	// delay(60000);
+	// write(0x00,DisSingle);
+	// delay(60000);
+	// write(0x01,DisTens);
+	// delay(60000);
+	// write(0x02,DisHunder);
+	// delay(60000);
+	// write(0x03,DisThous);
+	// delay(60000);
+	
+	// DisSingle=0;
+	// DisTens=0;
+	// DisHunder=0;
+	// DisThous=0;
+	
+	// SerialDataOutput(read(0x00));
+	// delay(60000);
+	// SerialDataOutput(read(0x01));
+	// delay(60000);
+	// SerialDataOutput(read(0x02));
+	// delay(60000);
+	// SerialDataOutput(read(0x03));
+	// delay(60000);
+	
 /* 	SerialDataOutput(0x06);// "1"
 	SerialDataOutput(0x5b);// "2"
 	SerialDataOutput(0x4f);// "3"
 	SerialDataOutput(0x66);// "4"
 	SerialDataOutput(0x06);// "1" */
 	
-	for (i=0;i<2;i++)
+/* 	for (i=0;i<2;i++)
 	{
 		SerialDataOutput(special[i+8]);
-	}
+	} */
 	
 	while (1)
 	{
@@ -94,14 +137,14 @@ void main(void)
 		
 		//SerialDataOutput(0x24);
 		
-		cna=CNUP;
+		//cna=CNUP;
 		//CNNUN=cna;
-		delay(50000);
-		cnb=CNUP;
-		if ((cna==1)&&(cnb==0))
-		{
-			num++;
-		}
+		// delay(50000);
+		//cnb=CNUP;
+		// if ((cna==1)&&(cnb==0))
+		// {
+			// num++;
+		// }
 
 		
 		
@@ -139,5 +182,5 @@ void Timer0_isr(void) interrupt 1 using 1
 	TH0=0x00;		/* Init value */
 	TL0=0x00;
 	
-	CNUP=~CNUP;
+	//CNUP=~CNUP;
 }
